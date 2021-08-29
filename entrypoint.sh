1a44cc5ba78e534f5ca529d72b908b49dcb48896
sksat <sksat@sksat.net>
2021-08-30 06:12:02 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
-#!/bin/ash
+#!/bin/sh
 
 B2_KEY_ID="$1"
 B2_KEY="$2"
-
 B2_BUCKET="$3"
-B2_SRC="$4"
 
-b2 authorize-account ${B2_APPKEY_ID} ${B2_APPKEY}
+SRC="$4"
+DEST="$5"
 
-b2 sync --delete --replaceNewer ${SOURCE_DIR} ${B2_BUCKET}
+echo "authorize"
+b2 authorize-account "${B2_KEY_ID}" "${B2_KEY}"
+
+echo "upload ${SRC} to ${B2_BUCKET} as ${DEST}"
+b2 upload-file ${B2_BUCKET} ${SRC} ${DEST}
 
 b2 clear-account
