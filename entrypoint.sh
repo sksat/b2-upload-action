58cdd362fd3779b9515f173561ddca9432c99f16
sksat <sksat@sksat.net>
2021-08-31 03:00:37 +0900

    TEMP: this must be fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,6 @@ b2 authorize-account "${B2_KEY_ID}" "${B2_KEY}"
 
 SHA1=$(sha1sum "${SRC}" | cut -d ' ' -f 1)
 echo "upload ${SRC} to ${B2_BUCKET} as ${DEST}"
-b2 upload-file ${B2_BUCKET} ${SRC} ${DEST} --sha1 "${SHA1}"
+b2 upload-file ${B2_BUCKET} ${SRC} ${DEST} --sha1 "${SHA1}hoge"
 
 b2 clear-account
