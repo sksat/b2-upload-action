648fb4479ce37c25df24a9226130e0363de6eb51
sksat <sksat@sksat.net>
2021-08-31 03:03:25 +0900

    use sha1 option (#4)
    * add: sha1 option
    
    * fix: build-image trigger
    
    * fix: test action trigger
    
    * TEMP: this must be fail
    
    * TEMP: this must be fail 2
    
    * TEMP: image branch
    
    * revert: TEMP
    
    * TEMP: update branch
    
    * fix: image
    
    * fix: sha1sum
    
    * TEMP: this must be fail
    
    * Revert "TEMP: this must be fail"
    
    This reverts commit 58cdd362fd3779b9515f173561ddca9432c99f16.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,8 @@ DEST="$5"
 echo "authorize"
 b2 authorize-account "${B2_KEY_ID}" "${B2_KEY}"
 
+SHA1=$(sha1sum "${SRC}" | cut -d ' ' -f 1)
 echo "upload ${SRC} to ${B2_BUCKET} as ${DEST}"
-b2 upload-file ${B2_BUCKET} ${SRC} ${DEST}
+b2 upload-file ${B2_BUCKET} ${SRC} ${DEST} --sha1 "${SHA1}"
 
 b2 clear-account
